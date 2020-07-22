ab93698453bbb29ff08f430a7cbc8b4b09db72c3
Abdullahi Mohamed <abdullahi.mohamed@consid.se>
2020-07-22 10:26:15 +0200

    Add project files.

---
+++ DailyNutrition/DataContext/DailyNutritionContext.cs
@@ -0,0 +1,15 @@
+﻿using DailyNutrition.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DailyNutrition.DataContext
+{
+    public class DailyNutritionContext : DbContext
+    {
+        public DailyNutritionContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Schema> Schemas { get; set; }
+    }
+}
---
+++ DailyNutrition/Models/Schema.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DailyNutrition.Models
+{
+    public class Schema
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public int FeedingDuration { get; set; }
+        public int FormulaAmount { get; set; }
+    }
+}
